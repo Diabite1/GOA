872203a27e891f7925add0cde8adc39bd983809f
Mate egriselashvili <mate.egriselashvili@gmail.com>
2024-02-16 20:59:47 +0400

    lesson4

---
+++ day4/lesson4(2).py
@@ -0,0 +1,10 @@
+my_name = "mate"
+
+my_surname = "egriselashvili"
+
+my_age = 16
+
+my_age_10_welshi = my_age + 10
+
+print("me","mqvia",my_name,my_surname,",","var",my_age,"wlis","da",
+      "10","welshi","viqnebi",my_age_10_welshi)
